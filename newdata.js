00cb75df2f0273ca08d78e596d0066c687b2c998
kgaur9586 <123kapilgaur9586@gmail.com>
2024-09-30 22:53:32 +0530

    Initial commit

---
+++ newdata.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%201.mp3',
+        artist: 'artist 1',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%201.png'
+    },
+    {
+        name: 'song 2',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%202.mp3',
+        artist: 'artist 2',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%202.png'
+    },
+    {
+        name: 'song 3',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%203.mp3',
+        artist: 'artist 3',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%2013.png'
+    },
+    {
+        name: 'song 4',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%204.mp3',
+        artist: 'artist 4',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%204.png'
+    },
+    {
+        name: 'song 5',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%205.mp3',
+        artist: 'artist 5',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%205.png'
+    },
+    {
+        name: 'song 6',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%206.mp3',
+        artist: 'artist 6',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%206.png'
+    },
+    {
+        name: 'song 7',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%207.mp3',
+        artist: 'artist 7',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%207.png'
+    },
+    {
+        name: 'song 8',
+        path: 'file:///D:/sweta%20sharma/Project/assets/musics/Song%208.mp3',
+        artist: 'artist 8',
+        cover: 'file:///D:/sweta%20sharma/Project/assets/images/cover%208.png'
+    },
+]
